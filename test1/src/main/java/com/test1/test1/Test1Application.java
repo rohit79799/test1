63700e9e7c3d57283acff3bc2c31639c784b1a73
rohit79799 <121388984+rohit79799@users.noreply.github.com>
2026-04-06 22:10:01 +0530

    Update Test1Application.java

--- test1/src/main/java/com/test1/test1/Test1Application.java
+++ test1/src/main/java/com/test1/test1/Test1Application.java
@@ -5,7 +5,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class Test1Application {
-	int x 100;
+	int x = 100;
+	int y = 200;
+	float m = 10.3f;
+	boolean istrue = true;
 
 	public static void main(String[] args) {
 		SpringApplication.run(Test1Application.class, args);
