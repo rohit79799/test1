4a6de1c6cc1e03effe5e143a4414f84cbcbe4e21
rohit79799 <121388984+rohit79799@users.noreply.github.com>
2026-04-06 22:26:08 +0530

    Update Test1Application.java

--- test1/src/main/java/com/test1/test1/Test1Application.java
+++ test1/src/main/java/com/test1/test1/Test1Application.java
@@ -9,6 +9,7 @@ public class Test1Application {
 	int y = 200;
 	float m = 10.3f;
 	boolean istrue = true;
+	char ch = 'r';
 
 	public static void main(String[] args) {
 		SpringApplication.run(Test1Application.class, args);
